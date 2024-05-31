841bd3e1e82e6a1d0cbdc328248f38aa992e1e40
Your Name <geckobaby8@gmail.com>
2024-05-31 13:57:39 +0400

    EOL changes

--- rps/src/App.jsx
+++ rps/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import paper from './img/paper.svg'
+import rock from './img/rock.svg'
+import scissors from './img/scissors.svg'
+import './style.sass'
 
 function App() {
 
   return (
     <>
-      <h1>Rock Paper Scisors!</h1>
+      <div id='game'>
+        <h1>Rock Paper Scisors!</h1>
+        <h2 id="counter">3</h2>
+        <div className="hands">
+          <img src={paper} alt="" />
+          <img src={rock} alt="" />
+        </div>
+        <div className="score">
+          <h3>0</h3>
+          <h3>0</h3>
+        </div>
+        <div className="choices">
+          <img src={rock} alt="" />
+          <img src={paper} alt="" />
+          <img src={scissors} alt="" />
+        </div>
+      </div>
     </>
   )
 }
